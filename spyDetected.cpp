a86c9bb494ef68bbe4633697c7ee5d3d1b594854
Suraj Biswas <surajbiswas182003@gmail.com>
2024-01-03 19:41:12 +0530

    added

---
+++ spyDetected.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define ll long long
+
+int main()
+{
+    int t;    
+    cin >> t;
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        for (int i = 0; i < n; i++) 
+        {
+            int count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    count++; 
+                } 
+            }
+            if (count == 1)
+            {
+                cout << i + 1<< "\n";  
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
